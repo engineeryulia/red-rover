bba7c5323c1421cd0d90fbee0be65936d0dcd91a
Yulia Lopatko <engineeryulia@gmail.com>
2023-05-07 17:50:06 -0600

    the second commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+
+import Hello from "../Hello/Hello"
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,10 @@
+import './Hello.css'
+function Hello() {
+  return (
+    <div className="Hello-block">
+      <h1 className="title">Hello, World!</h1>
+    </div>
+
+  )
+}
+export default Hello;
